983c2150b363f5eb1789f47b819ecc7d5346cb2e
orishameedah <orishameedah@gmail.com>
2023-07-19 11:03:57 +0100

    promptopia

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+// import { Metadata } from "next";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+
+const RootLayout = ({ children }) => {
+    return(
+        <html lang='en'>
+         <body>
+           <Provider>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+              <Nav />
+              {children}
+            </main>
+            </Provider> 
+         </body>
+        </html>
+    )
+};
+
+export default RootLayout;
